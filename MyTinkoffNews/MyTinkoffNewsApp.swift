a2827e9ea7cc3a0d6703fd7cdd084b5289ae11a6
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-11-25 19:18:41 +0300

    Initial Commit

---
+++ MyTinkoffNews/MyTinkoffNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTinkoffNewsApp.swift
+//  MyTinkoffNews
+//
+//  Created by Artyom Romanchuk on 25.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyTinkoffNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
