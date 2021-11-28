77199155ad04d84bcb4aee3b9ce7ae1fa453b5f0
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-11-28 11:31:09 +0300

    add Cell and more font bold mark

--- MyTinkoffNews/ContentView.swift
+++ MyTinkoffNews/ContentView.swift
@@ -11,27 +11,54 @@ struct ContentView: View {
     @State var posts = [Post]()
     
     var body: some View {
-        List(posts) { post in
-            Text("\(post.body)")
-        }
-        .task {
-                    do {
-                        posts = try await loadData()
-                    } catch {
-                        print(error)
-                    }
+        NavigationView {
+            List(posts) { post in
+                NavigationLink(destination: DetailView(posts: post)) {
+                    Text(post.title)
+                    Text("f")
+                        .padding()
+                        .font(.system(size: 30, weight: .heavy, design: .default))
+                        //.font(Font.body.bold())
+                        //.font(.custom("", size: 38))
+                        .foregroundColor(.red)
+                        
                 }
+            }
+            .task {
+                do {
+                    posts = try await loadData()
+                } catch {
+                    print(error)
+                }
+            }
+            .navigationBarHidden(false)
+            .navigationBarTitle("Tinkoff News", displayMode: .automatic)
+        }
+        
+        
+        
     }
     func loadData() async throws -> [Post] {
+        
+        let urlString = "https://jsonplaceholder.typicode.com/posts"
+        let url = URL(string: urlString)!
+        
+        let (data, _) = try await URLSession.shared.data(for: URLRequest(url: url))
+        return try JSONDecoder().decode([Post].self, from: data)
+    }
+    
+    
+}
+
+struct DetailView: View {
+    @State var posts: Post
+    
+    var body: some View {
+        VStack {
+            Text(posts.body).font(.title)
             
-            let urlString = "https://jsonplaceholder.typicode.com/posts"
-            let url = URL(string: urlString)!
-            
-            let (data, _) = try await URLSession.shared.data(for: URLRequest(url: url))
-            return try JSONDecoder().decode([Post].self, from: data)
         }
-
-
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
